45313487711b588a770082f14741081223e6524b
poldel2 <pol-del2@mail.ru>
2024-12-07 14:07:12 +0300

    Добавление связи между проектом и пользователем

--- src/main/java/com/example/TaskManager/model/Project.java
+++ src/main/java/com/example/TaskManager/model/Project.java
@@ -3,7 +3,9 @@ package com.example.TaskManager.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -25,4 +27,13 @@ public class Project {
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> tasks;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_user",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @ToString.Exclude
+    private Set<User> users = new HashSet<>();
 }
--- src/main/java/com/example/TaskManager/model/User.java
+++ src/main/java/com/example/TaskManager/model/User.java
@@ -1,6 +1,5 @@
 package com.example.TaskManager.model;
 
-
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.proxy.HibernateProxy;
@@ -35,24 +34,27 @@ public class User {
     @Column
     private boolean enabled;
 
-    @OneToMany(mappedBy = "author" , cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> createdTasks;
 
-    @OneToMany(mappedBy = "assignee" , cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "assignee", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Task> assignedTasks;
 
-    @OneToMany(mappedBy = "user" , cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Comment> comments;
 
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "username")
     private Set<Authority> authorities = new HashSet<>();
+
+    @ManyToMany(mappedBy = "users")
+    @ToString.Exclude
+    private Set<Project> projects = new HashSet<>();
 
     public Set<GrantedAuthority> getGrantedAuthorities() {
         return authorities.stream()
                 .map(authority -> (GrantedAuthority) authority)
                 .collect(Collectors.toSet());
     }
-
 
     @Override
     public final boolean equals(Object o) {
